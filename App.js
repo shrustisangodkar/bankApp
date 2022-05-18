17611cef312bb4ea37830ba6ebf6569432ee34f9
Shrusti Sangodkar <shrusti.sangodkar11@gmail.com>
2022-05-18 16:41:41 +0530

    Init commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Dashboard from './src/screens/Dashboard';
+import Login from './src/screens/Login';
+const navigator = createStackNavigator(
+  {
+    Login:Login,
+    Dashboard:Dashboard
   },
-});
+  {
+    initialRouteName: "Login",
+    defaultNavigationOptions: {
+      title: "App",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text,StyleSheet } from 'react-native';
+const Dashboard=()=>{
+    return(
+        <View>
+            <Text>Dashboard</Text>
+        </View>
+    )
+};
+
+const styles=StyleSheet.create({});
+
+export default Dashboard;
